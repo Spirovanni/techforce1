ddff80f0fb76b65d62a27c8d1270b1f1c93024f2
Xavier Martinez <blackshieldsx@gmail.com>
2019-12-08 14:46:16 -0800

    Finished Serializer and added views.py in api

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,1 +1,14 @@
 from rest_framework import serializers
+from .models import Organization, Rating
+
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organization
+        fields = ('id', 'org_name', 'org_description')
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ('id', 'stars', 'user')
