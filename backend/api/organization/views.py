4c9639e587782e7f84276bd4c36f56afbe4421b5
Xavier Martinez <blackshieldsx@gmail.com>
2019-12-08 15:51:14 -0800

    Craeted seperate views and folder for Organizations

---
+++ backend/api/organization/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from ..models import Organization, Rating
+from ..serializers import OrganizationSerializer, RatingSerializer
+
+
+class OrganizationViewSet(viewsets.ModelViewSet):
+    queryset = Organization.objects.all()
+    serializer_class = (OrganizationSerializer, )
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = (RatingSerializer, )
