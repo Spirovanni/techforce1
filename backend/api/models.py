aa9e57f148b7ee055fe3593c17b0ab8d6bd6f95e
Xavier Martinez <blackshieldsx@gmail.com>
2019-12-08 01:22:29 -0800

    Created organization Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import PermissionsMixin, AbstractBaseUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -35,3 +36,8 @@ class Organization(models.Model):
 class Rating(models.Model):
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        unique_together = (('user', 'organization'), )
+        index_together = (('user', 'organization'), )
