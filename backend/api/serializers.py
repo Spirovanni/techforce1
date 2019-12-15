13e741eb2ba68acb9ea52195871a0fbb84358862
Xavier Martinez <blackshieldsx@gmail.com>
2019-12-14 20:42:30 -0800

    Add no_of_ratings

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,10 +5,10 @@ from .models import Organization, Rating
 class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
-        fields = ('id', 'org_name', 'org_description')
+        fields = ('id', 'org_name', 'org_description', 'no_of_ratings')
 
 
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
-        fields = ('id', 'stars', 'user')
+        fields = ('id', 'stars', 'user', 'organization')
