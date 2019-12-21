3eca7d8da5198dd675509c8c6bd9aa0ed9704808
Xavier Martinez <blackshieldsx@gmail.com>
2019-12-21 09:30:31 -0800

    Bringing in the data

--- frontend/src/app/@core/interfaces/common/organizations.ts
+++ frontend/src/app/@core/interfaces/common/organizations.ts
@@ -1,3 +1,13 @@
 import { Observable } from 'rxjs';
 import { DataSource } from 'ng2-smart-table/lib/lib/data-source/data-source';
 import { Settings } from './settings';
+
+export interface Organization {
+  id: number;
+  org_name: string;
+  org_description: string;
+}
+
+export abstract class OrganizationData {
+  abstract 
+}
