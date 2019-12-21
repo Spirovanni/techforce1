ca3058f54b25ee38b309c4e1b49d09289b76d764
Xavier Martinez <blackshieldsx@gmail.com>
2019-12-21 09:57:40 -0800

    organization class
    organization

--- frontend/src/app/@core/interfaces/common/organizations.ts
+++ frontend/src/app/@core/interfaces/common/organizations.ts
@@ -2,6 +2,25 @@ import { Observable } from 'rxjs';
 import { DataSource } from 'ng2-smart-table/lib/lib/data-source/data-source';
 import { Settings } from './settings';
 
+export interface User {
+  id: number;
+  role: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  age: number;
+  userName: string;
+  picture: string;
+  address: Address;
+  settings: Settings;
+}
+
+export interface Address {
+  street: string;
+  city: string;
+  zipCode: string;
+}
+
 export interface Organization {
   id: number;
   org_name: string;
@@ -9,5 +28,23 @@ export interface Organization {
 }
 
 export abstract class OrganizationData {
-  abstract 
+  abstract get gridDataSource(): DataSource;
+  abstract getCurrentOrganization(): Observable<Organization>;
+  abstract list(pageNumber: number, pageSize: number): Observable<Organization[]>;
+  abstract get(id: number): Observable<Organization>;
+  abstract update(organization: Organization): Observable<Organization>;
+  abstract updateCurrent(organization: Organization): Observable<Organization>;
+  abstract create(organization: Organization): Observable<Organization>;
+  abstract delete(id: number): Observable<boolean>;
+}
+
+export abstract class UserData {
+  abstract get gridDataSource(): DataSource;
+  abstract getCurrentUser(): Observable<User>;
+  abstract list(pageNumber: number, pageSize: number): Observable<User[]>;
+  abstract get(id: number): Observable<User>;
+  abstract update(user: User): Observable<User>;
+  abstract updateCurrent(user: User): Observable<User>;
+  abstract create(user: User): Observable<User>;
+  abstract delete(id: number): Observable<boolean>;
 }
