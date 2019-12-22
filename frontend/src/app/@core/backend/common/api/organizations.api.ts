c6ce89ba1a0c8f55bafdd8ba8c5dbf663fc8e661
Xavier Martinez <blackshieldsx@gmail.com>
2019-12-22 14:24:22 -0800

    New datasource

--- frontend/src/app/@core/backend/common/api/organizations.api.ts
+++ frontend/src/app/@core/backend/common/api/organizations.api.ts
@@ -11,4 +11,52 @@ export class OrganizationsApi {
 
   constructor(private api: HttpService) {}
 
+  get organizationsDataSource(): DataSource {
+    return this.api.getServerDataSource(`${this.api.apiUrl}/${this.apiController}`);
+  }
+
+  orglist(pageNumber: number = 1, pageSize: number = 10): Observable<any[]> {
+    const params = new HttpParams()
+      .set('pageNumber', `${pageNumber}`)
+      .set('pageSize', `${pageSize}`);
+
+    return this.api.get(this.apiController, { params })
+      .pipe(map(data => data.map(item => {
+        const picture = `${this.api.apiUrl}/${this.apiController}/${item.id}/photo`;
+        return { ...item, picture };
+      })));
+  }
+
+  getCurrent(): Observable<any> {
+    return this.api.get(`${this.apiController}/current`)
+      .pipe(map(data => {
+        const picture = `${this.api.apiUrl}/${this.apiController}/${data.id}/photo`;
+        return { ...data, picture };
+      }));
+  }
+
+  get(id: number): Observable<any> {
+    return this.api.get(`${this.apiController}/${id}`)
+      .pipe(map(data => {
+        const picture = `${this.api.apiUrl}/${this.apiController}/${data.id}/photo`;
+        return { ...data, picture };
+      }));
+  }
+
+  delete(id: number): Observable<boolean> {
+    return this.api.delete(`${this.apiController}/${id}`);
+  }
+
+  add(item: any): Observable<any> {
+    return this.api.post(this.apiController, item);
+  }
+
+  updateCurrent(item: any): Observable<any> {
+    return this.api.put(`${this.apiController}/current`, item);
+  }
+
+  update(item: any): Observable<any> {
+    return this.api.put(`${this.apiController}/${item.id}`, item);
+  }
+
 }
