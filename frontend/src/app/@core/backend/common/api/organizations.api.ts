48462ab58f5949d64ab0bc4f04f886d3caaf3b9e
Xavier Martinez <blackshieldsx@gmail.com>
2019-12-22 11:05:34 -0800

    import { Observable } from 'rxjs';
    import { HttpService } from './http.service';

--- frontend/src/app/@core/backend/common/api/organizations.api.ts
+++ frontend/src/app/@core/backend/common/api/organizations.api.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpService } from './http.service';
 
 @Injectable()
-export class UsersApi {
+export class OrganizationsApi {
+  private readonly apiController: string = 'organizations';
+
+
 }
