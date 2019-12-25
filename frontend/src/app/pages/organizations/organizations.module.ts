d81f2595689fa7b73268be79a81ea600ecefbcac
Xavier Martinez <blackshieldsx@gmail.com>
2019-12-24 19:45:58 -0800

    import { ThemeModule } from '../../@theme/theme.module';

--- frontend/src/app/pages/organizations/organizations.module.ts
+++ frontend/src/app/pages/organizations/organizations.module.ts
@@ -2,3 +2,4 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 
+import { ThemeModule } from '../../@theme/theme.module';
