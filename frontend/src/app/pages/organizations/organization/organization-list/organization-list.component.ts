a2cd26231ab3b4c70d6e5f389edc158ea6cd2a08
Xavier Martinez <blackshieldsx@gmail.com>
2019-12-25 17:14:18 -0800

    Organizations
    <h6>organization-list works!</h6>

--- frontend/src/app/pages/organizations/organization/organization-list/organization-list.component.ts
+++ frontend/src/app/pages/organizations/organization/organization-list/organization-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationListComponent implements OnInit {
 
+  organizations = ['Chase Bank', 'Bank of America', 'Wells Fargo'];
   constructor() { }
 
   ngOnInit() {
